72a8973994a1f8ad70f9b30979288a9ce307d8e2
zhangyiluan <2528281406@qq.com>
2017-12-13 20:30:16 +0800

    bb

---
+++ app/src/main/java/android/com/gaoxiao/Fragment/cxkqxx.java
@@ -0,0 +1,16 @@
+package android.com.gaoxiao.Fragment;
+
+import android.app.Activity;
+import android.com.gaoxiao.R;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+
+public class cxkqxx extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cx3kqxx);
+    }
+}
